d6ff1aef94ae2abf79f2a0ae5c3b087a3e1b0c99
that_ quentin <QuentinMouilhaud@gmail.com>
2025-11-28 13:43:56 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8822072999-a4137",
+  "appId": "1:644784261357:web:80e04a3ce959e1ca53e4e4",
+  "apiKey": "AIzaSyBKWX4YCrQtFbYdB1XfdqOW3ymVI9fdMoI",
+  "authDomain": "studio-8822072999-a4137.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "644784261357"
+};
