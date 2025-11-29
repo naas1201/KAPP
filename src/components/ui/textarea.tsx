c8222c644398105643bd1705f73aa84699daffae
that_ quentin <QuentinMouilhaud@gmail.com>
2025-11-29 13:16:26 +0000

    https://console.cloud.google.com/cloud-build/builds;region=us-central1/b

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -1,47 +1,24 @@
-import * as React from 'react';
+import * as React from "react"
 
-import {cn} from '@/lib/utils';
-import { useMotionValue, useTransform, motion } from 'framer-motion';
+import { cn } from "@/lib/utils"
 
-const Textarea = React.forwardRef<HTMLTextAreaElement, React.ComponentProps<'textarea'>>(
-  ({className, ...props}, ref) => {
-    const x = useMotionValue(0);
-    const y = useMotionValue(0);
-    const rotateX = useTransform(y, [-100, 100], [-10, 10]);
-    const rotateY = useTransform(x, [-100, 100], [10, -10]);
+export interface TextareaProps
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
 
-    function handleMouseMove(event: React.MouseEvent<HTMLTextAreaElement>) {
-      const rect = event.currentTarget.getBoundingClientRect();
-      x.set(event.clientX - rect.left - rect.width / 2);
-      y.set(event.clientY - rect.top - rect.height / 2);
-    }
-
-    function handleMouseLeave() {
-      x.set(0);
-      y.set(0);
-    }
-
-
+const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
+  ({ className, ...props }, ref) => {
     return (
-      <motion.div 
-        className="relative"
-        style={{ perspective: 400 }}
-        onMouseMove={handleMouseMove}
-        onMouseLeave={handleMouseLeave}
-      >
-        <motion.textarea
-          style={{ rotateX, rotateY }}
-          className={cn(
-            'flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm shadow-lg',
-            className
-          )}
-          ref={ref}
-          {...props}
-        />
-      </motion.div>
-    );
+      <textarea
+        className={cn(
+          "flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    )
   }
-);
-Textarea.displayName = 'Textarea';
+)
+Textarea.displayName = "Textarea"
 
-export {Textarea};
+export { Textarea }
